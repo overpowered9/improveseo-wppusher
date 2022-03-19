84038ec96507ecbdfc1ab3cc091b396d5286f99e
Cloudflare Power Tools for Wordpress <63974184+barabasis@users.noreply.github.com>
2022-03-19 16:38:43 +0530

    Scheduled posts issues fixed

--- includes/ScheduledPosts.php
+++ includes/ScheduledPosts.php
@@ -6,21 +6,18 @@ if(get_option("improveseo_scheduled_last_execute_time") == false){
 if(get_option("improveseo_scheduled_execute_time") == false){
     update_option("improveseo_scheduled_execute_time", 20);
 }
-
-if(time() >= get_option("improveseo_scheduled_last_execute_time") + (60 * get_option("improveseo_scheduled_execute_time"))){
-	function pubMissedPosts() {
-        if (is_front_page() || is_single()) {
-            global $wpdb;
-            $now	=	gmdate("Y-m-d H:i:00");
-            $sql = "Select ID from {$wpdb->posts} where post_status='future' and post_date_gmt<='$now'";
-            $resulto = $wpdb->get_results($sql);
-            if($resulto) {
-                foreach( $resulto as $thisarr ) {
-                    wp_publish_post($thisarr->ID);
-                }
-            }
-		}
-	}
-	update_option('improveseo_scheduled_last_execute_time', time());
-	add_action('wp_head', 'pubMissedPosts');
+if(time() >= get_option("improveseo_scheduled_last_execute_time") + 300){
+	add_action('wp_head', 'improveseo_publish_missed_posts');
+}
+function improveseo_publish_missed_posts() {
+    global $wpdb;
+    $now	=	gmdate("Y-m-d H:i:00");
+    $sql = "Select ID from {$wpdb->posts} where post_status='future' and post_date_gmt<='$now'";
+    $resulto = $wpdb->get_results($sql);
+    if($resulto) {
+        foreach( $resulto as $thisarr ) {
+            wp_publish_post($thisarr->ID);
+        }
+    }
+    update_option('improveseo_scheduled_last_execute_time', time());
 }
