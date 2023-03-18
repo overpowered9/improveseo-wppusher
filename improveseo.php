cfa3aa1908788750179dc04578bb39d2e816903d
shamim-pro <shamimbdpro@gmail.com>
2023-03-18 23:39:08 +0600

    Preview Generate Issue Fixed

--- improveseo.php
+++ improveseo.php
@@ -4,9 +4,9 @@ Plugin Name: Improve SEO
 Plugin URI: 
 Description: Creates a large number of pages/posts and customize them to rank in Google.
 Author: Improve SEO Team
-Version: 2.0.11
+Version: 2.0.12
 */
-define("IMPROVESEO_VERSION", "2.0.11");
+define("IMPROVESEO_VERSION", "2.0.12");
 define('IMPROVESEO_ROOT', dirname(__FILE__));
 define('IMPROVESEO_DIR', untrailingslashit(plugin_dir_url( __FILE__ )));
 
