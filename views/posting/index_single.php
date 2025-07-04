9d75b6447a314b605abd671d55fc5a58ebc775ce
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-07-04 19:01:44 +0500

    centered

--- views/posting/index_single.php
+++ views/posting/index_single.php
@@ -106,7 +106,7 @@ if (isset($_POST['cat_name'])) {
 		</div>
 		<div class="create-ai-post">
 			<div class="create-ai">
-				<h2 class="title">What would you like to create?</h2>
+				
 				<div class="create-ai-col">
 					<a class="Posting__post-button" href="<?php echo admin_url("admin.php?page=improveseo_posting&action=create_post"); ?>">
 						<img src="<?php echo WT_URL . '/assets/images/latest-images/Mobile-UX-rafiki.png'; ?>"
