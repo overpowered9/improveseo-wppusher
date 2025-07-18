30a340493e617795ccc95a94150fa734804926e2
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-07-19 02:10:36 +0500

    Revert "flow fix for single and bulk"
    This reverts commit 9b192b4e611e1eea3392aa17c509333861901c60.

--- views/posting/index_single.php
+++ views/posting/index_single.php
@@ -108,7 +108,7 @@ if (isset($_POST['cat_name'])) {
 			<div class="create-ai">
 				
 				<div class="create-ai-col">
-					<a class="Posting__post-button" href="<?php echo admin_url("admin.php?page=improveseo_posting&action=create_post_single"); ?>">
+					<a class="Posting__post-button" href="<?php echo admin_url("admin.php?page=improveseo_posting&action=create_post"); ?>">
 						<img src="<?php echo WT_URL . '/assets/images/latest-images/Mobile-UX-rafiki.png'; ?>"
 							alt="Create Single AI Post">
 						<h3>Create Single AI Post</h3>
