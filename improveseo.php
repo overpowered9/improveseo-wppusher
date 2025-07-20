3b645e2e4a999672e27dbb05c35db0c52fed5cc7
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-07-21 02:17:08 +0500

    Update improveseo.php

--- improveseo.php
+++ improveseo.php
@@ -182,10 +182,15 @@ function add_my_media_button()
 
                 </select> &nbsp;';
 				
-	$html .= '
+	// $html .= '
+	// 			<div style=" display:flex; justify-content:end;">
+					
+	// 				<a type="button" style="margin-left:10px;" id="generate_ai_popup_open" class="styling_post_page_action_buttons2 styling_post_page_action_buttons" data-toggle="modal" data-target="#exampleModal"><img src="' . WT_URL . '/assets/images/latest-images/iconoir_sparks.svg" alt="iconoir_sparks">Generate AI Content</a>
+	// 			</div>';
+				$html .= '
 				<div style=" display:flex; justify-content:end;">
 					
-					<a type="button" style="margin-left:10px;" id="generate_ai_popup_open" class="styling_post_page_action_buttons2 styling_post_page_action_buttons" data-toggle="modal" data-target="#exampleModal"><img src="' . WT_URL . '/assets/images/latest-images/iconoir_sparks.svg" alt="iconoir_sparks">Generate AI Content</a>
+					<a type="button" style="margin-left:10px;" id="generate_ai_popup_open" class="styling_post_page_action_buttons2 styling_post_page_action_buttons" ><img src="' . WT_URL . '/assets/images/latest-images/iconoir_sparks.svg" alt="iconoir_sparks">Generate AI Content</a>
 				</div>';
 	
 
