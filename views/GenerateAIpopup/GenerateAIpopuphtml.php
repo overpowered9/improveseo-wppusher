b843457392a417aa285703a3efab24b52aa6c60c
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-07-21 01:06:22 +0500

    Update GenerateAIpopuphtml.php

--- views/GenerateAIpopup/GenerateAIpopuphtml.php
+++ views/GenerateAIpopup/GenerateAIpopuphtml.php
@@ -1180,7 +1180,7 @@ $image_url2 = $plugin_url . 'assets/images/loaderr.gif';
          const postType = "<?php echo $post_type; ?>";
          console.log(postType);
         
-        // Open Generate AI Popup Modal
+        
       // Open Generate AI Popup Modal
 if (generate_ai_popup_open) {
     generate_ai_popup_open.addEventListener('click', () => {
