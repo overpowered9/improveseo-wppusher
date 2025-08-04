f930935da09aea24a8afa3ce1aafa145c9cd5b4f
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-08-04 20:59:43 +0500

    fix

--- autoloader.php
+++ autoloader.php
@@ -1,7 +1,27 @@
-<?php
-
-include_once 'includes/ImproveSEO/Autoloader.php';
-
-new ImproveSEO\Autoloader();
-
-session_start();
+<?php
+
+
+
+
+
+include_once 'includes/ImproveSEO/Autoloader.php';
+
+
+
+
+
+new ImproveSEO\Autoloader();
+
+
+
+
+
+// session_start();
+// Add this hook instead:
+add_action('init', 'improveseo_start_session');
+
+function improveseo_start_session() {
+    if (!session_id()) {
+        session_start();
+    }
+}
