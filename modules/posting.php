3ece76b4126fdee8ba936aa46d1ff5ddbb499e9e
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-08-12 22:45:00 +0500

    create_si

--- modules/posting.php
+++ modules/posting.php
@@ -136,8 +136,16 @@ function improveseo_posting(){
 
 			)) && !isset($_POST['draft'])) {
 
-
-				wp_redirect(admin_url('admin.php?page=improveseo_posting&action=create_post'));
+				global $ai_modal_type;
+        
+       			 // Check ai_modal_type and redirect accordingly
+       			 if (isset($ai_modal_type) && $ai_modal_type === 'single') {
+            		wp_redirect(admin_url('admin.php?page=improveseo_posting&action=create_post_single'));
+        			} else {
+            				wp_redirect(admin_url('admin.php?page=improveseo_posting&action=create_post'));
+       						
+						}
+				
 
 
 				exit;
