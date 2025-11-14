3ac827c6d2ec21e67411444adda2abdaa29a6dc7
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-11-15 00:57:31 +0500

    Update GenerateAIpopuphtml.php

--- views/GenerateAIpopup/GenerateAIpopuphtml.php
+++ views/GenerateAIpopup/GenerateAIpopuphtml.php
@@ -681,7 +681,7 @@ global $ai_modal_type;
                     <div class="percent_multi">
                         <div class="step_multi">
                             <div class="circle_multi">4</div>
-                            <p>SEO <br>Visuals</p>
+                            <p>Select <br>category</p>
                         </div>
                     </div>
                     <!-- Step 5 -->
