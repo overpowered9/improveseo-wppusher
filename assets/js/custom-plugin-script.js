f43f1d7aac885caee85e2b8287070228876634d3
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-11-15 10:25:16 +0500

    Revert "resolve issues of keyword mapping"
    This reverts commit 446cb56f97598b20be789f2ea1b50cd0466e519b.

--- assets/js/custom-plugin-script.js
+++ assets/js/custom-plugin-script.js
@@ -982,14 +982,6 @@ jQuery(document).ready(function ($) {
     var formData = new FormData(form);
 
     formData.append("action", "multiPostData");
-    
-    // Debug: Log keyword_images data
-    console.log('Form submission - checking keyword_images:');
-    var keywordImageInputs = jQuery('input[name^="keyword_images"]');
-    console.log('Found ' + keywordImageInputs.length + ' keyword_images inputs');
-    keywordImageInputs.each(function() {
-      console.log(jQuery(this).attr('name') + ' = ' + (jQuery(this).val().substring(0, 50) + '...'));
-    });
 
     $.ajax({
       url: ajaxurl,
@@ -1558,11 +1550,7 @@ function attachKeywordImageHandlers() {
 }
 
 function updateHiddenInputs() {
-  // Clear all existing keyword_images hidden inputs
-  jQuery('input[name^="keyword_images"]').remove();
-  
-  var processedCount = 0;
-  var totalCards = jQuery('.keyword-card.has-image').length;
+  var hiddenInputsHtml = '';
   
   jQuery('.keyword-card').each(function() {
     var keywordIndex = jQuery(this).data('keyword-index');
@@ -1573,25 +1561,19 @@ function updateHiddenInputs() {
       var reader = new FileReader();
       reader.onload = function(e) {
         // Create hidden input with base64 image data
-        jQuery('#hiddenInputs').append(
-          '<input type="hidden" name="keyword_images[' + keywordIndex + '][keyword]" value="' + escapeHtml(keyword) + '">' +
-          '<input type="hidden" name="keyword_images[' + keywordIndex + '][image]" value="' + e.target.result + '">'
-        );
-        processedCount++;
-        
-        // Debug log when all images are processed
-        if (processedCount === totalCards) {
-          console.log('All ' + totalCards + ' keyword images processed and added to form');
+        var existingInput = jQuery('input[name="keyword_images[' + keywordIndex + '][image]"]');
+        if (existingInput.length) {
+          existingInput.val(e.target.result);
+        } else {
+          jQuery('#hiddenInputs').append(
+            '<input type="hidden" name="keyword_images[' + keywordIndex + '][keyword]" value="' + escapeHtml(keyword) + '">' +
+            '<input type="hidden" name="keyword_images[' + keywordIndex + '][image]" value="' + e.target.result + '">'
+          );
         }
       };
       reader.readAsDataURL(fileInput.files[0]);
     }
   });
-  
-  // If no images with files, log that too
-  if (totalCards === 0) {
-    console.log('No keyword images to process');
-  }
 }
 
 function updateProgress() {
