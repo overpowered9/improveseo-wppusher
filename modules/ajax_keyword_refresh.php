c8b78238ffc6882ad51c9d22c1eb124ce20bce8c
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-11-25 06:55:40 +0500

    Revert "text area with db"
    This reverts commit 4a09876b21c131bb4ab5fc88ed53f10411c517bc.

--- modules/ajax_keyword_refresh.php
+++ modules/ajax_keyword_refresh.php
@@ -35,12 +35,12 @@ function improveseo_get_keyword_list_data() {
     }
     
     $list = $wpdb->get_row($wpdb->prepare(
-        "SELECT list FROM {$wpdb->prefix}improveseo_lists WHERE id = %d",
+        "SELECT keywords FROM {$wpdb->prefix}improveseo_lists WHERE id = %d",
         $list_id
     ));
     
     if ($list) {
-        $keywords = $list->list; // Column is named 'list', not 'keywords'
+        $keywords = $list->keywords;
         $keyword_array = array_filter(explode("\n", $keywords));
         $keyword_count = count($keyword_array);
         $keyword_min = $keyword_count * 3;
