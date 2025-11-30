9bbb2c0a416839183b40a398bfdec9cd9c3b2b34
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-11-30 14:40:13 +0500

    Revert "test/actions menu"
    This reverts commit bef687644cbd188abae23a91efc663e2aca2018c.

--- views/bulkprojects/index.php
+++ views/bulkprojects/index.php
@@ -174,21 +174,16 @@ if (isset($_GET['post_preview'])) {
 
 													<span class="edit">
 														<a class="popup-link"
-															href="<?php echo admin_url("admin.php?page=improveseo_bulkprojects&action=export_urls&id={$project->id}&name={$project->name}&noheader=true") ?>">
+															href="<?php admin_url("admin.php?page=improveseo_projects&action=export_urls&id={$project->id}&name={$project->name}&noheader=true") ?>"
+															disabled>
 															Export a list of all posts/pages URLs
 														</a>
 													</span>
 													<span class="edit">
 														<a class="popup-link"
-															href="<?php echo admin_url('admin.php?page=improveseo_bulkprojects&action=stop&id=' . $project->id . '&noheader=true')  ?>">
+															href="<?php admin_url('admin.php?page=improveseo_projects&action=stop&id=' . $project->id . '&noheader=true')  ?>">
 															Stop process
 														</a>
-													</span>
-													<span class="edit">
-														<a class="popup-link" class="submitdelete"
-															href="<?php echo admin_url('admin.php?page=improveseo_bulkprojects&action=delete_posts&id=' . $project->id . '&noheader=true')  ?>"
-															onclick="return confirm('This action will delete all generated posts/pages but keep the project')">Delete
-															Posts</a>
 													</span>
 													<span class="edit">
 														<a class="popup-link" class="submitdelete" target="_blank"
@@ -197,7 +192,7 @@ if (isset($_GET['post_preview'])) {
 													</span>
 													<span class="trash">
 														<a class="popup-link delete-link" class="submitdelete"
-															href="<?php echo admin_url('admin.php?page=improveseo_bulkprojects&action=delete&id=' . $project->id . '&noheader=true')  ?>"
+															href="<?php  admin_url('admin.php?page=improveseo_projects&action=delete&id=' . $project->id . '&noheader=true')  ?>"
 															onclick="return confirm('This action will delete project and all generated posts/pages')">Delete
 															project and all posts/pages</a>
 													</span>
