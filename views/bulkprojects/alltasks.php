ca8c78702ae9f983fb3522b83aed0cb80c16ce36
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-12-01 18:55:21 +0500

    Update alltasks.php

--- views/bulkprojects/alltasks.php
+++ views/bulkprojects/alltasks.php
@@ -160,13 +160,15 @@ $url .= $_SERVER['REQUEST_URI'];
 						<tbody>
 							<?php foreach ($projects as $key => $project): ?>
 								<tr <?= $highlight == $project->id ? ' class="WHProject--highlight"' : '' ?>>
-									<td data-label="Name">
-										<label class="checkbox style-c">
-											<input id="cb-select-<?php echo $project->id; ?>" type="checkbox"
-												name="project_ids[]" value="<?php echo $project->id; ?>">
-											<div class="checkbox__checkmark"></div>
-										</label>
-										<h4> <?= $project->keyword_name ?> </h4>
+									<td data-label="Name" style="vertical-align: top;">
+										<div style="display: flex; align-items: flex-start;">
+											<label class="checkbox style-c" style="margin-top: 2px;">
+												<input id="cb-select-<?php echo $project->id; ?>" type="checkbox"
+													name="project_ids[]" value="<?php echo $project->id; ?>">
+												<div class="checkbox__checkmark"></div>
+											</label>
+											<h4 style="margin: 0 0 0 8px; word-break: break-word; white-space: pre-line;"> <?= $project->keyword_name ?> </h4>
+										</div>
 									</td>
 									<td data-label="Language"><?= $project->content_lang ?></td>
 									<td data-label="Size"><?= $project->nos_of_words ?></td>
