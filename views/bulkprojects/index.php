ab2da0e4b105146e4d0f117f94d260d334a1c59c
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-12-09 03:55:32 +0500

    Update index.php

--- views/bulkprojects/index.php
+++ views/bulkprojects/index.php
@@ -146,7 +146,7 @@ if (isset($_GET['post_preview'])) {
 									?> </td>
 								<td data-label="Status" class="status finished"><?php
 								if ($project->state == 'Stopped')
-									echo '<p class="post-st" style="color: #ff4d4f; font-weight: 600;">Canceled</p>';
+									echo '<p class="post-st" style="color: #ff4d4f; font-weight: 600; font-size: 14px;" >Canceled</p>';
 								elseif ($project->state == 'Finished')
 									echo '<p class="post-fd">Finished</p>';
 								elseif ($project->state == 'Draft')
