024fffb88c90349b7927044198f17fa93d1420e8
overpowered9 <117577891+overpowered9@users.noreply.github.com>
2025-12-09 12:27:00 +0500

    Update GenerateAIpopuphtml.php

--- views/GenerateAIpopup/GenerateAIpopuphtml.php
+++ views/GenerateAIpopup/GenerateAIpopuphtml.php
@@ -874,8 +874,8 @@ global $ai_modal_type;
 
                         <div id="AI_image_div" class="col-md-12" style="display:none;">
                             <div id="ai-image-display"></div>
-                            <div class="form-group col-md-12" style="margin: 0 0 0 40%;" id="AIrefreshOption">
-                                <button type="button" class="style_next_button_in_popup" onclick="return refreshAIImage()">Regenerate AI Image</button>
+                            <div class="form-group col-md-12" style="display: flex; justify-content: center; margin: 20px 0;" id="AIrefreshOption">
+                                <button type="button" class="style_next_button_in_popup" onclick="return refreshAIImage()" style="padding: 12px 30px !important;">Regenerate AI Image</button>
                                 <!-- <i class="fa fa-refresh" aria-hidden="true" onclick="return refreshAIImage()"
                                 style="cursor:pointer;"></i> -->
                             </div>
